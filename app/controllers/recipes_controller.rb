93ec977950df3488ec1d192b151f09a4c0cc38c3
kasumikishimoto <kasumikishimot00@gmail.com>
2021-10-13 20:30:50 +0900

    Active Storageの導入

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,5 +1,19 @@
 class RecipesController < ApplicationController
+
   def index
     
+  end
+
+  def new
+    
+  end
+
+  def create
+    
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:title, :material, :text, :image).merge(user_id: current_user.id)
   end
 end
