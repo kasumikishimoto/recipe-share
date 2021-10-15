963a7ff256b9429a2fef7930025849d82cc3d10f
kasumikishimoto <kasumikishimot00@gmail.com>
2021-10-15 11:25:38 +0900

    新規投稿 バリデーション・アソシエーション

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,10 @@
 class Recipe < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  with_options presence: true do
+    validates :title
+    validates :material
+    validates :text
+  end
 end
