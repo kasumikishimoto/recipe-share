9835bf9d9b709d7999f0aa11b81e44c432e3a38f
kasumikishimoto <kasumikishimot00@gmail.com>
2021-10-16 16:29:25 +0900

    recipe controllerの追加

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,5 @@
 class RecipesController < ApplicationController
+  before_action :move_to_index, expect:[:index, :show]
 
   def index
     @recipes = Recipe.order('created_at DESC')
@@ -21,9 +22,37 @@ class RecipesController < ApplicationController
     @recipe = Recipe.find(params[:id])
   end
 
+  def edit
+    @recipe = Recipe.find(params[:id])
+    redirect_to root_path unless current_user.id == @recipe.user_id
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(recipe_params)
+    if @recipe.save
+      redirect_to recipe_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    recipe = Recipe.find(params[:id])
+    if recipe.destroy
+      redirect_to root_path
+    end
+  end
+
   private
 
   def recipe_params
     params.require(:recipe).permit(:title, :material, :text, :category_id, :time_require_id, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to root_path
+    end
   end
 end
