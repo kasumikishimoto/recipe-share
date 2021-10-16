983bea4d198874f025ef223449f4c46ea87fa94d
kasumikishimoto <kasumikishimot00@gmail.com>
2021-10-16 15:23:33 +0900

    詳細ページの実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,7 @@
 class RecipesController < ApplicationController
 
   def index
-    @recipes = Recipe.order("created_at DESC")
+    @recipes = Recipe.order('created_at DESC')
   end
 
   def new
@@ -11,15 +11,19 @@ class RecipesController < ApplicationController
   def create
     @recipe = Recipe.new(recipe_params)
     if @recipe.save
-      reirect_to root_path
+      redirect_to root_path
     else
       render :new
     end
   end
 
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
   private
-  
+
   def recipe_params
-    params.require(:recipe).permit(:title, :material, :text, :image, :category_id, :time_require_id).merge(user_id: current_user.id)
+    params.require(:recipe).permit(:title, :material, :text, :category_id, :time_require_id, :image).merge(user_id: current_user.id)
   end
 end
