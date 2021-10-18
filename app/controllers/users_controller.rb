cf48c021c82ce87f814e205c607cf7d7abb5f4f4
kasumikishimoto <kasumikishimot00@gmail.com>
2021-10-18 14:54:53 +0900

    user詳細ページの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,8 @@
 class UsersController < ApplicationController
 
-  def index
-    @user = User.find(params[:id])
-    @recipe = @user.recipes
-  end
-
   def show
     @user = User.find(params[:id])
+    @recipe = @user.recipes
   end
 
 end
