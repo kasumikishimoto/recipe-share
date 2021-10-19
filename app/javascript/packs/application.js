2193b444b6cd5fbfc178806722d128caff0b36b0
kasumikishimoto <kasumikishimot00@gmail.com>
2021-10-19 19:27:20 +0900

    ActionCableを用いたコメント機能の実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("channels/comment_channel")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
