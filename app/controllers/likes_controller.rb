2db95f17aab8addbcaff463109651db6bb32a955
kasumikishimoto <kasumikishimot00@gmail.com>
2021-11-03 17:13:26 +0900

    likes_controllerの修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,12 +1,16 @@
 class LikesController < ApplicationController
+  before_action :recipe_params
 
   def create
     Like.create(user_id: current_user.id, recipe_id: params[:id])
-    redirect_to recipe_path
   end
 
   def destroy
     Like.find_by(user_id: current_user.id, recipe_id: params[:id]).destroy
-    redirect_to recipe_path
+  end
+
+  private
+  def recipe_params
+    @recipe = Recipe.find(params[:id])
   end
 end
