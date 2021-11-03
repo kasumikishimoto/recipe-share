46b6ce00f5c0cfc8dbdc1fa5520cf6cc41f58e30
kasumikishimoto <kasumikishimot00@gmail.com>
2021-11-03 14:51:38 +0900

    likes_controllerメソッドの追加

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,6 +2,12 @@ class LikesController < ApplicationController
 
   def create
     Like.create(user_id: current_user.id, recipe_id: params[:id])
-    redirect_to recipes_path
+    redirect_to recipe_path
+  end
+
+  def destroy
+    Like.find_by(user_id: current_user.id, recipe_id: params[:id]).destroy
+    redirect_to recipe_path
+    
   end
 end
